1c4a563b4782a0b3122aaed68ce33ff59ff8e870
Cristen <andrecristenibirama@gmail.com>
2020-04-17 22:01:26 -0300

    Configuração Heroku

--- src/Pummax/Configuration/DataBase.php
+++ src/Pummax/Configuration/DataBase.php
@@ -4,16 +4,16 @@ namespace Pummax\Configuration;
 
 class DataBase
 {
-    const URL_SITE = "http://atleticaceavi.com.br/";
+    const URL_SITE = "https://atletica-ceavi.herokuapp.com/";
 
     public static function getConnectionConfiguration(){
         //Postgresql
         $conn = array(
-            'dbname' => 'atletica',
-            'user' => 'postgres',
+            'dbname' => 'da24cdp7e3jitc',
+            'user' => 'kjjrxucfxxaxjo',
             'port' => '5432',
-            'password' => '123456',
-            'host' => 'localhost',
+            'password' => '971058513fff46baded61d894e563fc2a68a759770dc7951d1536e8fe4473a08',
+            'host' => 'ec2-52-73-247-67.compute-1.amazonaws.com',
             'driver' => 'pdo_pgsql',
         );
         //MySql
