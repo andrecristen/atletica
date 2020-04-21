25f0337cfc05072314ef2507ec6542c3dbdb9ebc
Cristen <andrecristenibirama@gmail.com>
2020-04-21 17:25:09 -0300

    Adicionado os campos na consulta de carteirinha.

--- src/Repository/CarteiraRepository.php
+++ src/Repository/CarteiraRepository.php
@@ -9,4 +9,18 @@ use Pummax\Repository\BaseRepository;
 class CarteiraRepository extends BaseRepository
 {
 
+    public function createQueryConsulta(){
+        $alias = $this->getEntityAlias();
+        $query = $this->getEntityManager()->createQueryBuilder();
+        $query->addSelect($alias);
+        $query->addSelect('Curso');
+        $query->addSelect('Usuario');
+        $query->addSelect('Pessoa');
+        $query->from($this->getEntityName(), $alias, null);
+        $query->join('Carteira.curso', 'Curso');
+        $query->join('Carteira.usuario', 'Usuario');
+        $query->join('Usuario.pessoa', 'Pessoa');
+        return $query;
+    }
+
 }
--- src/View/Grid/CarteiraGrid.php
+++ src/View/Grid/CarteiraGrid.php
@@ -5,6 +5,8 @@ namespace View\Grid;
 
 
 use Model\Carteira;
+use Model\Curso;
+use Model\Pessoa;
 use Model\Usuario;
 use Pummax\View\Grid\AbstractGrid;
 use Pummax\View\Grid\Action;
@@ -21,7 +23,13 @@ class CarteiraGrid extends AbstractGrid
     public function createColumns()
     {
         $this->addColumn(new Column('#', 'id', Column::TYPE_NUMBER, $this->getModelNameBase(),true, '5%'));
-        $this->addColumn(new Column('Data Vencimento', 'dataVencimento', Column::TYPE_DATE, $this->getModelNameBase(),true));
+        $this->addColumn(new Column('Data Vencimento', 'dataVencimento', Column::TYPE_DATE, $this->getModelNameBase(),true, '10%'));
+        $this->addColumn(new Column('#Curso', 'id', Column::TYPE_NUMBER, Curso::class,true, '5%'));
+        $this->addColumn(new Column('Curso', 'nome', Column::TYPE_TEXT, Curso::class,true, '10%'));
+        $this->addColumn(new Column('Login', 'login', Column::TYPE_TEXT, Usuario::class,true, '10%'));
+        $this->addColumn(new Column('Nome', 'nome', Column::TYPE_TEXT, Pessoa::class,true, '20%'));
+        $this->addColumn(new Column('Sobrenome', 'sobrenome', Column::TYPE_TEXT, Pessoa::class,true, '40%'));
+        $this->addColumn(new Column('CPF', 'cpf', Column::TYPE_TEXT, Pessoa::class,true, '10%'));
     }
 
     public function createActions()
