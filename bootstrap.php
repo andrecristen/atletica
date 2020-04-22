c4aba27491d2288474b3cda3032a3e2c9ed0bb4e
Cristen <andrecristenibirama@gmail.com>
2020-04-22 20:49:47 -0300

    Config data

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
 require_once "vendor/autoload.php";
-
+date_default_timezone_set("America/Sao_Paulo");
 $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src/Model"), \Pummax\Configuration\DataBase::isDevMode());
 $entityManager = EntityManager::create(\Pummax\Configuration\DataBase::getConnectionConfiguration(), $config);
 $conection = new \Pummax\DataBase\Connection();
